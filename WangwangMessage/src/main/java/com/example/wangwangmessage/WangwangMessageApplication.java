f67523aa46a240a2b0aec0d05ff853f912c92e2a
ArTist <1308666843@qq.com>
2024-08-22 17:03:50 +0800

    create project of wangwang message

---
+++ WangwangMessage/src/main/java/com/example/wangwangmessage/WangwangMessageApplication.java
@@ -0,0 +1,13 @@
+package com.example.wangwangmessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WangwangMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WangwangMessageApplication.class, args);
+	}
+
+}
---
+++ WangwangMessage/src/test/java/com/example/wangwangmessage/WangwangMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wangwangmessage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WangwangMessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
